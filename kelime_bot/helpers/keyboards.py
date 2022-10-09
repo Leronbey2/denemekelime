865eff731b3d7a6518daecaf82d073d3dbd0ab95
Leronbey2 <115078425+Leronbey2@users.noreply.github.com>
2022-10-09 22:29:30 +0300

    Update keyboards.py

--- kelime_bot/helpers/keyboards.py
+++ kelime_bot/helpers/keyboards.py
@@ -2,7 +2,7 @@ from pyrogram.types import InlineKeyboardButton
 from pyrogram.types import InlineKeyboardMarkup
 
 kanal = InlineKeyboardMarkup([
-    [InlineKeyboardButton("⚔️ Oyun Ve Film Botumuz ⚔️" , url= "t.me/inekgame_bot")]
+    [InlineKeyboardButton("⚔️ Luiz Bots ⚔️" , url= "t.me/LuizBots")]
 ])
 
 
